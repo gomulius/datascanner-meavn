6dc61f1dccb760765630a508b1b1d55a46840487
gomulius <42356346+gomulius@users.noreply.github.com>
2018-09-18 14:07:28 +0100

    mongoose added....
    ... tested and it works perfectly. Had a little problem with testing it - had to use a standalone postman app as it was giving me a posibillity to submit body as a  Application/JSON - otherwise the parameters will not be retrieve by node.js

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const fs = require("fs");
+
+const app = express();
+const router = express.Router();
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(cors());
+
+const DBurl = "mongodb://localhost:27017";
+
+// connect to mongoDB and raf-test cluster
+mongoose.connect(DBurl + "/raf-test", function(){
+    console.log('Connection to the DB has been made');
+}) 
+.catch(err => {
+    console.error('App starting error', err.stack);
+    process.exit;
+})
+
+// Include controllers
+fs.readdirSync("controllers").forEach(function (file){
+    if(file.substr(-3) == ".js") {
+        const route = require("./controllers/" + file);
+        route.controller(app);
+    }
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'API initialized!' });
+});
+
+const port = process.env.API_PORT || 8081;
+
+app.use('/', router);
+app.listen(port, function() {
+    console.log(`appi running on port ${port}`);
+});
